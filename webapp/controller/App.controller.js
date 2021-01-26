e5c7c4ab6ef04089b186c862ea34621a16017221
Serge Stavrovski <seistof@Serges-MacBook-Pro.local>
2021-01-26 22:17:54 +0300

    Translatable text

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -2,8 +2,9 @@ sap.ui.define([
     'sap/ui/core/mvc/Controller',
     'sap/m/MessageToast',
     'sap/ui/model/json/JSONModel',
+    'sap/ui/model/resource/ResourceModel',
 
-], function(Controller, MessageToast, JSONModel) {
+], function(Controller, MessageToast, JSONModel, ResourceModel) {
     'use strict';
 
     return Controller.extend('sap.ui.demo.try.App', {
@@ -17,10 +18,25 @@ sap.ui.define([
 
             const oModel = new JSONModel(oData);
             this.getView().setModel(oModel);
+
+            // set i18n Model on View
+            const i18nModel = new ResourceModel({
+                bundleName: 'sap.ui.demo.try.i18n.i18n',
+                supportedLocales: [''],
+                fallbackLocale: '',
+            });
+            this.getView().setModel(i18nModel, 'i18n');
         },
         onShowHello: function() {
-            // Show alert
-            MessageToast.show('Hello!');
+            // read message from i18n Model
+            const oBundle = this.getView().getModel('i18n').getResourceBundle();
+            const sRecipient = this.getView().
+                getModel().
+                getProperty('/recipient/name');
+            const sMsg = oBundle.getText('helloMsg', [sRecipient]);
+
+            // Show message
+            MessageToast.show(sMsg);
         },
     });
 });
